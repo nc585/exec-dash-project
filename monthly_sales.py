0208deb57134a6aec85b5297afab5d6ba9b4117f
nc585 <nc585@georgetown.edu>
2019-02-11 17:56:09 -0500

    insert chart

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,47 +1,58 @@
 # monthly_sales.py
 
-# TODO: import some modules and/or packages here
-
 import os
-import csv
-import itertools
-from operator import itemgetter
+import pandas
+import matplotlib.pyplot as plt
 
-csv_file_name = "sales-201803.csv" # a relative file path
+# adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py
+
+def to_usd(my_price):
+    return f"${my_price:,.2f}"
+
+csv_file_name = "sales-201803.csv" # TODO: change from relative to allow user input
+
+# adapted from https://github.com/s2t2/exec-dash-starter-py/commit/525446a5850d211bb78dfe1cb3ffb42ea4b3c9ad#diff-2bc9303c4e0187b3363d76974cc2fc8c
+
+csv_file_path = os.path.join(os.path.dirname(__file__),"data",csv_file_name)
+csv_data = pandas.read_csv(csv_file_path)
+
+# rows = []
+
+# with open(csv_file_path, "r") as csv_file:
+#     reader = csv.DictReader(csv_file)
+#     for d in reader:
+#         rows.append(dict(d))
+
+monthly_total = csv_data["sales price"].sum()
+
+top_sellers = [
+    {"name": "Button-Down Shirt", "monthly_sales": 6960.34},
+    {"name": "Super Soft Hoodie", "monthly_sales": 1875.0},
+    {"name": "Khaki Pants", "monthly_sales": 1602.0},
+    {"name": "Vintage Logo Tee", "monthly_sales": 941.05},
+    {"name": "Brown Boots", "monthly_sales": 250.0},
+    {"name": "Sticker Pack", "monthly_sales": 216.0},
+    {"name": "Baseball Cap", "monthly_sales": 156.31}
+] # TODO: get from CSV data instead
+
+# sales_prices = [float(row["sales price"]) for row in rows]
+# total_sales = sum(sales_prices) #format this into USD 
 
 # adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py 
 
-csv_file_path = os.path.join("data",csv_file_name)
+# product_sales = []
+# sorted_rows = sorted(rows, key=itemgetter("product"))
+# rows_by_product = itertools.groupby(sorted_rows, key=itemgetter("product"))
 
-rows = []
+# for product, product_rows in rows_by_product:
+#     monthly_sales = sum([float(row["sales price"])for row in product_rows])
+#     product_sales.append({"name": product, "monthly_sales": monthly_sales})
 
-with open(csv_file_path, "r") as csv_file:
-    reader = csv.DictReader(csv_file)
-    for d in reader:
-        rows.append(dict(d))
-
-sales_prices = [float(row["sales price"]) for row in rows]
-total_sales = sum(sales_prices) #format this into USD 
-
-# adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py 
-
-product_sales = []
-sorted_rows = sorted(rows, key=itemgetter("product"))
-rows_by_product = itertools.groupby(sorted_rows, key=itemgetter("product"))
-
-for product, product_rows in rows_by_product:
-    monthly_sales = sum([float(row["sales price"])for row in product_rows])
-    product_sales.append({"name": product, "monthly_sales": monthly_sales})
-
-sorted_product_sales = sorted(product_sales, key=itemgetter("monthly_sales"), reverse=True)
-top_sellers = sorted_product_sales[0:3]
+# sorted_product_sales = sorted(product_sales, key=itemgetter("monthly_sales"), reverse=True)
+# top_sellers = sorted_product_sales[0:3]
 
 month = "MARCH" #pull this from file data values
 year = 2018 #pull this from file or data values
-
-# adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py
-def to_usd(my_price):
-    return f"${my_price:,.2f}"
 
 # TODO: write some Python code here to produce the desired functionality...
 
@@ -53,8 +64,12 @@ print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-#print("TOTAL MONTHLY SALES: $12,000.71")
-print(f"TOTAL MONTHLY SALES: {to_usd(total_sales)}")
+print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
+
+# rank = 1
+# for d in top_sellers:
+#     print("  " + str(rank)+ ") " + d["name"]+ ": " + to_usd(d["monthly_sales"]))
+#     rank = rank + 1
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
@@ -65,4 +80,18 @@ print("  3) etc.")
 print("-----------------------")
 print("VISUALIZING THE DATA...")
 
-# make the charts here 
+# adapted from: https://github.com/s2t2/exec-dash-starter-py/commit/a0adfbe1af1b867889fdece114226cbd57657872#diff-2bc9303c4e0187b3363d76974cc2fc8c
+
+chart_title = "Top Selling Products (March 2018)" #TODO: get month and year
+
+sorted_products = []
+sorted_sales = []
+
+for d in top_sellers:
+    sorted_products.append(d["name"])
+    sorted_sales.append(d["monthly_sales"])
+
+plt.bar(sorted_products, sorted_sales)
+plt.ylabel("Monthly Sales (USD)")
+plt.xlabel("Products")
+plt.show()

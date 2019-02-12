5cf2d7407c8ea291e960b2ebcb0df8a415bbb9bd
nc585 <nc585@georgetown.edu>
2019-02-11 23:02:38 -0500

    date

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,7 +1,8 @@
 # monthly_sales.py
 
+import operator
 import os
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py
@@ -9,19 +10,22 @@ import matplotlib.pyplot as plt
 def to_usd(my_price):
     return f"${my_price:,.2f}"
 
-csv_file_name = "sales-201803.csv" # TODO: change from relative to allow user input
+# adapted from shopping cart project
+
+while True:
+    # notes from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/os.md
+    print(os.listdir("/Users/Nina/Documents/Python/exec-dash-project/data")) 
+    
+    csv_file_name = input("Please input the file name from the directory above in the format of sales-YYYYMM.csv: ")
+    if csv_file_name == "sales-201803.csv": #os.listdir("/Users/Nina/Documents/Python/exec-dash-project/data"):
+        break #make the above a reference  
+    if not csv_file_name == os.listdir("/Users/Nina/Documents/Python/exec-dash-project/data"):
+        print("We could not find your file. Please enter the exact file name from the directory above.")
 
 # adapted from https://github.com/s2t2/exec-dash-starter-py/commit/525446a5850d211bb78dfe1cb3ffb42ea4b3c9ad#diff-2bc9303c4e0187b3363d76974cc2fc8c
 
 csv_file_path = os.path.join(os.path.dirname(__file__),"data",csv_file_name)
-csv_data = pandas.read_csv(csv_file_path)
-
-# rows = []
-
-# with open(csv_file_path, "r") as csv_file:
-#     reader = csv.DictReader(csv_file)
-#     for d in reader:
-#         rows.append(dict(d))
+csv_data = pd.read_csv(csv_file_path)
 
 monthly_total = csv_data["sales price"].sum()
 
@@ -30,29 +34,31 @@ product_names = csv_data["product"]
 unique_product_names = product_names.unique()
 unique_product_names = unique_product_names.tolist()
 
-top_sellers = [] # TODO: get from CSV data instead
-
+top_sellers = [] 
 for product_name in unique_product_names:
-    product_monthly_sales = 100.00 #TODO calculate this
+    matching_rows = csv_data[csv_data["product"] == product_name]
+    product_monthly_sales = matching_rows["sales price"].sum()
     top_sellers.append({"name": product_name, "monthly_sales": product_monthly_sales})
-# sales_prices = [float(row["sales price"]) for row in rows]
-# total_sales = sum(sales_prices) #format this into USD 
 
-# adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/csv_solution_further.py 
+top_sellers = sorted(top_sellers, key=operator.itemgetter("monthly_sales"), reverse=True)
 
-# product_sales = []
-# sorted_rows = sorted(rows, key=itemgetter("product"))
-# rows_by_product = itertools.groupby(sorted_rows, key=itemgetter("product"))
+# adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/pandas_explore.py
 
-# for product, product_rows in rows_by_product:
-#     monthly_sales = sum([float(row["sales price"])for row in product_rows])
-#     product_sales.append({"name": product, "monthly_sales": monthly_sales})
+def month_lookup(month):
+    year_month={'01':'January','02':'February','03':'March','04':'April',
+	'05':'May','06':'June','07':'July','08':'August','09':'September','10':'October',
+	'11':'November', '12':'December'}
+    return year_month[month]
 
-# sorted_product_sales = sorted(product_sales, key=itemgetter("monthly_sales"), reverse=True)
-# top_sellers = sorted_product_sales[0:3]
+month = month_lookup(csv_file_name[-6:-4]) #pull this from file data values
+year = int(csv_file_name[6:10]) #pull this from file or data values
 
-month = "MARCH" #pull this from file data values
-year = 2018 #pull this from file or data values
+# products = csv_data["product"].unique()
+# products.sort()
+# sales_price = csv_data.groupby(csv_data["product"]).sum()
+# sales_price_col = list("sales price")
+# total_price_by_prod = pd.DataFrame({"products":products, "sales price":sales_price_col})
+# total_price_by_prod = total_price_by_prod.sort_values(by=["sales price"],ascending=False)
 
 # TODO: write some Python code here to produce the desired functionality...
 
@@ -66,10 +72,10 @@ print("CRUNCHING THE DATA...")
 print("-----------------------")
 print(f"TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
 
-# rank = 1
-# for d in top_sellers:
-#     print("  " + str(rank)+ ") " + d["name"]+ ": " + to_usd(d["monthly_sales"]))
-#     rank = rank + 1
+rank = 1
+for d in top_sellers:
+    print("  " + str(rank)+ ") " + d["name"]+ ": " + to_usd(d["monthly_sales"]))
+    rank = rank + 1
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
